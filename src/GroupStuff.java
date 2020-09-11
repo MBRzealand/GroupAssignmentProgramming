fead8cbe6f95bd4a08597d8275cad6bfc75e62fb
Mikkel B. Rasmussen <mikk24f6@edu.easj.dk>
2020-09-11 11:02:15 +0200

    fjernet et psvm for meget

--- src/GroupStuff.java
+++ src/GroupStuff.java
@@ -1,12 +1,7 @@
 public class GroupStuff {
 
     public static void main(String[] args) {
-
         System.out.println("Mikkel her");
-
-    }
-
-    public static void main(String[] args) {
         System.out.println("Mathias");
         System.out.println("Anders Hjordrup Larsen");
 
